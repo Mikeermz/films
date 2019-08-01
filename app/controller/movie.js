dfe6fa8dd99af46d8670fd61839932e134bc79c4
Mikee Ramirez <miguel.ramirez@nowports.com>
2019-07-31 21:46:28 -0500

    separete controller
    Signed-off-by: Mikee Ramirez <miguel.ramirez@nowports.com>

--- app/controller/movie.js
+++ app/controller/movie.js
@@ -6,8 +6,71 @@ const createMovie = (req, resp) => {
   newMovie.save((error, movie) => {
     error ? resp.status(409).send(error) : resp.status(201).send(movie)
   })
-}
+};
+
+const getMovies = (req, resp) => {
+  Movie.find({isActive: true}).exec()
+      .then( ( movies ) => resp.status(200).send({movies}))
+      .catch(( error ) => resp.status(409).send(error))
+};
+
+// One Movie
+const getMovieById = (req, resp) => {
+  const { id } = req.params;
+  Movie.findById(id).exec()
+      .then( ( movie ) => movie
+                          ? resp.status(200).send({movie})
+                          : resp.status(404).send({message: 'Not Found'}) )
+      .catch(( error ) => resp.status(409).send(error))
+};
+
+// One Movie
+const getMovieByName = (req, resp) => {
+  const { title } = req.query
+  Movie.findOne({title}).exec()
+      .then( ( movie ) => movie
+                          ? resp.status(200).send({movie})
+                          : resp.status(404).send({message: 'Not Found'}) )
+      .catch(( error ) => resp.status(409).send(error))
+};
+
+// Update Movie
+const updateMovie = (req, resp) => {
+  console.log(req.body);
+  const { id } = req.params;
+  Movie.findByIdAndUpdate(id, {$set: req.body}, {new: true}).exec()
+      .then( ( movie ) => movie
+                          ? resp.status(200).send({movie})
+                          : resp.status(404).send({message: 'Not Found'}) )
+      .catch(( error ) => resp.status(409).send(error))
+};
+
+// Delete Logic Movie
+const deleteLogicMovie = (req, resp) => {
+  const { id } = req.params;
+  Movie.findByIdAndUpdate(id, {$set: {isActive: false}}, {new: true}).exec()
+      .then( ( movie ) => movie
+                          ? resp.status(200).send({movie})
+                          : resp.status(404).send({message: 'Not Found'}) )
+      .catch(( error ) => resp.status(409).send(error))
+};
+
+// Delete Forever
+const deleteMovieForEver = (req, resp) => {
+  const { id } = req.params;
+  Movie.findByIdAndDelete(id).exec()
+      .then( ( movie ) => movie
+                          ? resp.status(200).send({movie})
+                          : resp.status(404).send({message: 'Not Found'}) )
+      .catch(( error ) => resp.status(409).send(error))
+};
 
 module.exports = {
-  createMovie
+  createMovie,
+  getMovies,
+  getMovieById,
+  getMovieByName,
+  updateMovie,
+  deleteLogicMovie,
+  deleteMovieForEver
 };
--- app/routes/index.js
+++ app/routes/index.js
@@ -1,7 +1,11 @@
 const express = require('express');
-const { Movie } = require('../model/Movie');
 const { homeController } = require('../controller/home');
-const { createMovie } = require('../controller/movie');
+const {
+  createMovie, getMovies, getMovieById,
+  getMovieByName, updateMovie, deleteLogicMovie,
+  deleteMovieForEver
+} = require('../controller/movie');
+
 const router = express.Router();
 
 // Home router
@@ -11,61 +15,21 @@ router.get('/', homeController);
 router.post('/movie', createMovie);
 
 //  All Movies
-router.get('/movies', (req, resp) => {
-  Movie.find({isActive: true}).exec()
-      .then( ( movies ) => resp.status(200).send({movies}))
-      .catch(( error ) => resp.status(409).send(error))
-});
+router.get('/movies', getMovies );
 
 // One Movie
-router.get('/movie/:id', (req, resp) => {
-  const { id } = req.params;
-  Movie.findById(id).exec()
-      .then( ( movie ) => movie
-                          ? resp.status(200).send({movie})
-                          : resp.status(404).send({message: 'Not Found'}) )
-      .catch(( error ) => resp.status(409).send(error))
-});
+router.get('/movie/:id', getMovieById);
 
-// One Movie
-router.get('/search', (req, resp) => {
-  const { title } = req.query
-  Movie.findOne({title}).exec()
-      .then( ( movie ) => movie
-                          ? resp.status(200).send({movie})
-                          : resp.status(404).send({message: 'Not Found'}) )
-      .catch(( error ) => resp.status(409).send(error))
-});
+// Search by name
+router.get('/search', getMovieByName);
 
 // Update Movie
-router.patch('/movie/:id', (req, resp) => {
-  console.log(req.body);
-  const { id } = req.params;
-  Movie.findByIdAndUpdate(id, {$set: req.body}, {new: true}).exec()
-      .then( ( movie ) => movie
-                          ? resp.status(200).send({movie})
-                          : resp.status(404).send({message: 'Not Found'}) )
-      .catch(( error ) => resp.status(409).send(error))
-});
+router.patch('/movie/:id', updateMovie);
 
 // Delete Logic Movie
-router.delete('/movie/:id', (req, resp) => {
-  const { id } = req.params;
-  Movie.findByIdAndUpdate(id, {$set: {isActive: false}}, {new: true}).exec()
-      .then( ( movie ) => movie
-                          ? resp.status(200).send({movie})
-                          : resp.status(404).send({message: 'Not Found'}) )
-      .catch(( error ) => resp.status(409).send(error))
-});
+router.delete('/movie/:id', deleteLogicMovie);
 
 // Delete Forever
-router.delete('/movie/delete/:id', (req, resp) => {
-  const { id } = req.params;
-  Movie.findByIdAndDelete(id).exec()
-      .then( ( movie ) => movie
-                          ? resp.status(200).send({movie})
-                          : resp.status(404).send({message: 'Not Found'}) )
-      .catch(( error ) => resp.status(409).send(error))
-});
+router.delete('/movie/delete/:id', deleteMovieForEver);
 
 module.exports = router;
